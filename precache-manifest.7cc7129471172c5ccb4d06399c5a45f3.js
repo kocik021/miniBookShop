9678d2dce8b1a3c64515ed79a55b053dda754972
Jarek Kocik <kocik1908@gmail.com>
2020-02-12 15:18:00 +0000

    Updates

---
+++ precache-manifest.7cc7129471172c5ccb4d06399c5a45f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c788e4a2bc5a9eef25839456521ece18",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "3b728ad662116b320d66",
+    "url": "/nazwa/static/css/main.b7aaab63.chunk.css"
+  },
+  {
+    "revision": "11a1ecc1f53ab3c7aaba",
+    "url": "/nazwa/static/js/2.1b4f2669.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/nazwa/static/js/2.1b4f2669.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b728ad662116b320d66",
+    "url": "/nazwa/static/js/main.cd7b92b1.chunk.js"
+  },
+  {
+    "revision": "650433fe3f2caef4ce7b",
+    "url": "/nazwa/static/js/runtime-main.b6bce8ba.js"
+  }
+]);
